5be241f04a5e2463a14033d47ac05f446207bfad
aleks tkach <ale.tkachenko@gmail.com>
2015-02-22 00:36:38 -0600

    In Initializer spec: cleaned up needless require statements, redefined Kernel #puts and #print, to get rid of outputs/noise while running specs

--- spec/lib/initializer_spec.rb
+++ spec/lib/initializer_spec.rb
@@ -1,8 +1,4 @@
-# require File.expand_path('../../lib/straight-server/initializer', File.dirname(__FILE__))
 require 'fileutils'
-require_relative '../../lib/straight-server/random_string'
-require_relative '../../lib/straight-server/initializer'
-require_relative '../../lib/straight-server/config'
 require_relative '../../lib/straight-server'
 
 RSpec.describe StraightServer::Initializer do
@@ -13,36 +9,48 @@ RSpec.describe StraightServer::Initializer do
   end
 
   before(:each) do
-    remove_temp_dir
-    templates_dir = File.expand_path('../../templates', File.dirname(__FILE__))
-    ENV['HOME']   = File.expand_path('../temp', File.dirname(__FILE__))
-    @test_class_object = StraightServer::TestInitializerClass.new
+    # redefining Kernel #puts and #print, to get rid of outputs/noise while running specs
+    module Kernel 
+      alias :original_puts :puts
+      alias :original_print :print
+      def puts(s); end
+      def print(s); end
+    end
+    remove_tmp_dir
+    @templates_dir = File.expand_path('../../templates', File.dirname(__FILE__))
+    ENV['HOME']   = File.expand_path('../tmp', File.dirname(__FILE__))
+    @initializer = StraightServer::TestInitializerClass.new
     StraightServer::Initializer::ConfigDir.set!
   end
 
   after(:each) do
-    remove_temp_dir
+    # reverting redefinition of Kernel #puts and #print made in before block
+    module Kernel 
+      alias :puts :original_puts
+      alias :print :original_print
+    end
+    remove_tmp_dir
   end
 
-  
-  it "creates config files" do
+  # as #create_config_files method contains #exit method we need to rescue SystemExix: exit() error
+  # and at the same time its good to assert that method execution went well, which we do in rescue
+  let(:create_config_files) do
     begin
-      @test_class_object.create_config_files
+      @initializer.create_config_files
     rescue Exception => e
       expect(e.status).to eq 0 
     end
+  end
+
+  it "creates config files" do
+    create_config_files
     expect(File.exist?(StraightServer::Initializer::ConfigDir.path)).to eq true
-    created_config_files = Dir.glob(File.join(File.expand_path('../temp', File.dirname(__FILE__)), '**', '*'), File::FNM_DOTMATCH).select { |f| File.file? f }
+    created_config_files = Dir.glob(File.join(File.expand_path('../tmp', File.dirname(__FILE__)), '**', '*'), File::FNM_DOTMATCH).select { |f| File.file? f }
     expect(created_config_files.size).to eq 3
     created_config_files.each do |file|
-      case file
-      when file.match(/.*\.straight\/addons.yml\Z/)
-        extect(File.read(file)).to eq File.read(templates_dir + '/addons.yml')
-      when file.match(/.*\.straight\/config.yml\Z/)
-        extect(File.read(file)).to eq File.read(templates_dir + '/config.yml')
-      when file.match(/.*\.straight\/server_secret\Z/)
-        extect(File.read(file).size).to eq 16
-      end
+      expect(File.read(file)).to eq File.read(@templates_dir + '/addons.yml') if file.match(/.*\.straight\/addons.yml\Z/)
+      expect(File.read(file)).to eq File.read(@templates_dir + '/config.yml') if file.match(/.*\.straight\/config.yml\Z/)
+      expect(File.read(file).scan(/\w+/).join.size).to eq 16                  if file.match(/.*\.straight\/server_secret\Z/)
     end
   end
 
@@ -51,21 +59,15 @@ RSpec.describe StraightServer::Initializer do
       adapter: 'sqlite',
       name: 'straight.db', 
     }
-    begin
-      @test_class_object.create_config_files
-    rescue SystemExit => e
-    end
-    @test_class_object.connect_to_db
+    create_config_files
+    @initializer.connect_to_db
     expect(StraightServer.db_connection.test_connection).to be true
   end
 
   it "creates logger" do
     log_configs = StraightServer::Config.logmaster = { 'log_level' => 'WARN', 'file' => 'straight.log' }
-    begin
-      @test_class_object.create_config_files
-    rescue SystemExit => e
-    end
-    expect(@test_class_object.create_logger).to be_kind_of(StraightServer::Logger)
+    create_config_files
+    expect(@initializer.create_logger).to be_kind_of(StraightServer::Logger)
   end
 
   it "runs migrations" do
@@ -73,18 +75,15 @@ RSpec.describe StraightServer::Initializer do
       adapter: 'sqlite',
       name: 'straight.db', 
     }
-    begin
-      @test_class_object.create_config_files
-    rescue SystemExit => e
-    end
-    @test_class_object.connect_to_db
+    create_config_files
+    @initializer.connect_to_db
     expect(Sequel::Migrator).to receive(:run).with(any_args)
-    expect( -> { @test_class_object.run_migrations }).not_to raise_error
+    expect( -> { @initializer.run_migrations }).not_to raise_error
   end
 
-  def remove_temp_dir
-    if Dir.exist?(File.expand_path('../temp/', File.dirname(__FILE__)))
-      FileUtils.rm_r(File.expand_path('../temp/', File.dirname(__FILE__)))
+  def remove_tmp_dir
+    if Dir.exist?(File.expand_path('../tmp/', File.dirname(__FILE__)))
+      FileUtils.rm_r(File.expand_path('../tmp/', File.dirname(__FILE__)))
     end      
   end
 
