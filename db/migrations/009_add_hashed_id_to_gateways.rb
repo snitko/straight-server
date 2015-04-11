4e4f94b3b864e2d135718b5e5b51c5365f384723
Roman Snitko <roman.snitko@gmail.com>
2015-04-12 02:12:46 +0700

    Add Gateway#hashed_id generation to the respective migration

--- db/migrations/009_add_hashed_id_to_gateways.rb
+++ db/migrations/009_add_hashed_id_to_gateways.rb
@@ -3,6 +3,9 @@ Sequel.migration do
   up do
     add_column :gateways, :hashed_id, String
     add_index  :gateways, :hashed_id, unique: true
+    StraightServer.db_connection[:gateways].each do |g|
+      g.update(hashed_id: OpenSSL::HMAC.digest('sha256', StraightServer::Config.server_secret, g[:id].to_s).unpack("H*").first)
+    end
   end
 
   down do
