5120a180a21291293d0579b1374d8063c7ca9dc0
Roman Snitko <roman.snitko@gmail.com>
2015-04-13 04:12:15 +0700

    Fix: conflicts due to Dropbox acting all weird, ignore this commit

--- db/migrations/009_add_hashed_id_to_gateways.rb
+++ db/migrations/009_add_hashed_id_to_gateways.rb
@@ -2,7 +2,7 @@ Sequel.migration do
 
   up do
     add_column :gateways, :hashed_id, String
-    add_index  :gateways, :hashed_id, unique: true
+    add_index  :gateways, :hashed_id
     if defined?(StraightServer)
       StraightServer.db_connection[:gateways].each do |g|
         g.update(hashed_id: OpenSSL::HMAC.digest('sha256', StraightServer::Config.server_secret, g[:id].to_s).unpack("H*").first)
