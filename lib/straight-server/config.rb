2716d461919611180b0376cf07ae2f40b814d122
Roman Snitko <roman.snitko@gmail.com>
2015-04-14 17:41:34 +0700

    Add port option to config file. Doesn't affect actual port yet, but helpful in determening what port the server runs on

--- lib/straight-server/config.rb
+++ lib/straight-server/config.rb
@@ -3,7 +3,7 @@ module StraightServer
   class Config
 
     class << self
-      attr_accessor :db, :gateways_source, :gateways, :logmaster, :server_secret, :count_orders, :environment, :redis, :check_order_status_in_db_first
+      attr_accessor :db, :gateways_source, :gateways, :logmaster, :server_secret, :count_orders, :environment, :redis, :check_order_status_in_db_first, :port
     end
 
   end
