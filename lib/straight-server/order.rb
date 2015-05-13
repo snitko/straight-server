528d93c882f9a8276e00c0bb3daa55c34cca202c
Roman Snitko <roman.snitko@gmail.com>
2015-05-13 17:44:27 +0700

    Return keychain_id and last_keychain_id from Order#to_http_params

--- lib/straight-server/order.rb
+++ lib/straight-server/order.rb
@@ -78,7 +78,7 @@ module StraightServer
     end
 
     def to_http_params
-      "order_id=#{id}&amount=#{amount}&status=#{status}&address=#{address}&tid=#{tid}"
+      "order_id=#{id}&amount=#{amount}&status=#{status}&address=#{address}&tid=#{tid}&keychain_id=#{keychain_id}&last_keychain_id=#{@gateway.last_keychain_id}"
     end
 
     def before_create
