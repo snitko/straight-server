f2d7216046ecad15e117a8bb218ff04e858a203f
Roman Snitko <roman.snitko@gmail.com>
2015-05-13 18:22:33 +0700

    Rename order_id into keychain_id in client example

--- examples/client/client.js
+++ examples/client/client.js
@@ -5,7 +5,7 @@ jQuery(function($) {
       url: '/gateways/' + $("input[name=gateway_id]").val() + '/orders',
       type: 'POST',
       dataType: 'json',
-      data: { amount: $("input[name=amount]").val(), signature: $("input[name=signature]").val(), order_id :$("input[name=order_id]").val() },
+      data: { amount: $("input[name=amount]").val(), signature: $("input[name=signature]").val(), keychain_id :$("input[name=keychain_id]").val() },
       success: function(response) {
         window.location = '/pay/' + response.payment_id
       }
