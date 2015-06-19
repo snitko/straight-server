3353825f5807e56142e6c1209013097c99f82240
AlexanderPavlenko <AlexanderPavlenko@users.noreply.github.com>
2015-06-19 12:30:57 +0300

    Cashila addon needs to store configuration in StraightServer::Config

--- lib/straight-server/config.rb
+++ lib/straight-server/config.rb
@@ -1,37 +1,34 @@
+require 'ostruct'
+
 module StraightServer
 
-  class Config
+  # :db
+  # :gateways_source
+  # :gateways
+  # :logmaster
+  # :server_secret
+  # :count_orders
+  # :environment
+  # :redis
+  # :check_order_status_in_db_first
+  # :port
+  # :blockchain_adapters
+  # :expiration_overtime
+  # :reuse_address_orders_threshold
+  # :throttle
 
-    class << self
-      attr_accessor :db,
-                    :gateways_source,
-                    :gateways,
-                    :logmaster,
-                    :server_secret,
-                    :count_orders,
-                    :environment,
-                    :redis,
-                    :check_order_status_in_db_first,
-                    :port,
-                    :blockchain_adapters,
-                    :expiration_overtime,
-                    :reuse_address_orders_threshold,
-                    :throttle
-
-      def [](key_chain)
-        key_chain = key_chain.to_s.split('.')
-        config = self.public_send(key_chain.shift)
-        key_chain.each do |key|
-          if config.kind_of?(Hash)
-            config = config[key] || config[key.to_sym]
-          else
-            return
-          end
+  class << (Config = OpenStruct.new)
+    def [](key_chain)
+      key_chain = key_chain.to_s.split('.')
+      config    = self.public_send(key_chain.shift)
+      key_chain.each do |key|
+        if config.kind_of?(Hash)
+          config = config[key] || config[key.to_sym]
+        else
+          return
         end
-        config
       end
+      config
     end
-
   end
-
 end
